2c380d3a515c46ba993bad44e798db87c9feb507
Fatemeh <fatemeh.heidari-soureshjani@polymtl.ca>
2021-05-09 11:28:29 -0400

    BundleBondaddress error

--- packages/react-app/src/utils/bundlebondcontract.js
+++ packages/react-app/src/utils/bundlebondcontract.js
@@ -1,6 +1,5 @@
 import Web3 from "web3";
 import { BundleBond } from "../abi/BundleBond.js";
-import { BondleBondAddress } from "../contracts/BundleBond.address.js";
 
 const OPTIONS = {
   defaultBlock: "latest",
