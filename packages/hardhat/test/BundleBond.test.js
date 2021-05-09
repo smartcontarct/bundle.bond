bd5805dfa37476f5ae250711aa34061a3b9603dd
aysha8665 <53833830+aysha8665@users.noreply.github.com>
2021-05-09 07:53:45 +0430

    Update BundleBond.test.js

--- packages/hardhat/test/BundleBond.test.js
+++ packages/hardhat/test/BundleBond.test.js
@@ -1,5 +1,5 @@
 const { ethers } = require("hardhat");
-const { use, expect } = require("chai");
+const { use, expect, assert } = require("chai");
 const { solidity } = require("ethereum-waffle");
 let catchRevert = require("./exceptionsHelpers.js").catchRevert
 const { waffle } = require("hardhat");
@@ -33,15 +33,85 @@ describe("Bundle Bond", function () {
     });
   });
 
+  describe("addAdmin()", function () {
+    it("Should be able to add a new Admin", async function () {
+      const adminName = "Admin Name";
+      const [admin, teamOwner, customer]=await ethers.getSigners();
+      await bundleBondContract.addAdmin(adminName,admin.address);
+      expect(await bundleBondContract.getAdminName(admin.address)).to.equal(adminName);
+    });
+
+    // it("Should be able to detect an Admin", async function () {
+    //   const adminName = "Admin Name";
+    //   const [admin, teamOwner, customer]=await ethers.getSigners();
+    //   await bundleBondContract.addAdmin(adminName,admin.address);
+    //   //let receipt=await bundleBondContract.connect(admin).isAdmin();
+    //   //console.log(receipt.logs[0]);
+    //   await bundleBondContract.isAdmin();
+    //   expect('isAdmin').to.be.calledOnContractWith(bundleBondContract, [admin.address]);
+    //   //expect(bundleBondContract.connect(admin.address).isAdmin()).to.be.equal(true);
+    // });
+
+    it("Should be able to edit Admin name", async function () {
+      const adminName = "Admin Name";
+      const [admin, teamOwner, customer]=await ethers.getSigners();
+      await bundleBondContract.addAdmin(adminName,teamOwner.address);
+      const adminName2 = "Admin Name2";
+      await bundleBondContract.editAdminName(adminName2,admin.address);
+      expect(await bundleBondContract.getAdminName(admin.address)).to.equal(adminName2);
+    });
+
+  });
+
     describe("addTeamOwner()", function () {
-      it("Should be able to set a new purpose", async function () {
+
+      it("Should be able to add a new TeamOwner", async function () {
         const teamOwnerName = "Team Name";
         const [admin, teamOwner, customer]=await ethers.getSigners();
         await bundleBondContract.addTeamOwner(teamOwnerName,teamOwner.address);
-        expect(await bundleBondContract.getTeamOwnerName(teamOwner)).to.equal(teamOwnerName);
+        expect(await bundleBondContract.getTeamOwnerName(teamOwner.address)).to.equal(teamOwnerName);
       });
+
+      it("Should be able to edit a TeamOwner name", async function () {
+        const teamOwnerName = "Team Name";
+        const [admin, teamOwner, customer]=await ethers.getSigners();
+        await bundleBondContract.addTeamOwner(teamOwnerName,teamOwner.address);
+        const teamOwnerName2 = "Team Name2";
+        await bundleBondContract.editTeamOwnerName(teamOwnerName2,teamOwner.address);
+        expect(await bundleBondContract.getTeamOwnerName(teamOwner.address)).to.equal(teamOwnerName2);
+      });
+
+
     });
 
+
+    describe("addTeam()", function () {
+
+      it("Should be able to add a new Team NFT", async function () {
+        const teamOwnerName = "TeamName";
+        const [admin, teamOwner, customer]=await ethers.getSigners();
+        const id=await bundleBondContract.addTeam(teamOwner.address,teamOwnerName,"metadata");
+        //console.log(id);
+        //expect(id).toString().to.equal(1);
+        //expect(await bundleBondContract.getTeamCount()).to.equal(1);
+        const receipt=await bundleBondContract.getTeamById(0);
+        //console.log(receipt);
+        //expect(receipt[1]).to.equal(teamOwner.address);
+      });
+
+
+
+      // it("Should be able to get a TeamNFT", async function () {
+      //   const teamOwnerName = "Team Name";
+      //   const [admin, teamOwner, customer]=await ethers.getSigners();
+      //   await bundleBondContract.addTeamOwner(teamOwnerName,teamOwner.address);
+      //   const teamOwnerName2 = "Team Name2";
+      //   await bundleBondContract.editTeamOwnerName(teamOwnerName2,teamOwner.address);
+      //   expect(await bundleBondContract.getTeamOwnerName(teamOwner.address)).to.equal(teamOwnerName2);
+      // });
+
+
+    });
 
   });
 
