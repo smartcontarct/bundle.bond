0ecf55e1b67bec662454b955659e33f64ac361af
aysha8665 <53833830+aysha8665@users.noreply.github.com>
2021-05-09 22:46:14 +0430

    Update BundleBond.sol

--- packages/hardhat/contracts/BundleBond.sol
+++ packages/hardhat/contracts/BundleBond.sol
@@ -9,14 +9,12 @@ interface ITeamNFT {
         string memory _name,
         address _teamOwner
     ) public returns (uint256);
-    
+
     function mintTeamToken(
         address teamOwner,
         string name,
-        string memory _temDes,
-        string memory _teamLoc,
         string metadata //isTeamOwner(teamOwner)
-    ) public returns (uint256 _teamId) ;
+    ) public returns (uint256);
 
     function evaluateTeam(uint256 teamId, address projectAddress) public;
 
@@ -46,10 +44,7 @@ interface IWeekNFT {
     function mint721(
         address _teamContract,
         uint256 _teamTokenId,
-        address userAddress,
-        string metadata,
-        uint256 _WeekNo,
-        uint256 _Year
+        address userAddress
     ) public returns (uint256);
 
     function assignNFTToTeam(
@@ -64,25 +59,23 @@ interface IWeekNFT {
         address bidder,
         uint256 price
     ) public;
-
-    function transferWeekNFTOnBid(
+        function transferWeekNFTOnBid(
         uint256 weekTokenId,
         address from,
         address to
     );
-
     function getWeekById(uint256 weekId)
         public
         view
         returns (
-            uint256 id,
-            uint256 teamId,
-            address weekOwner,
-            bool exists,
-            string WeekUrl,
-            uint256 price,
-            uint256 WeekNo,
-            uint256 Year
+        uint256 id,
+        uint256 teamId,
+        address weekOwner,
+        bool exists,
+        string WeekUrl,
+        uint256 price,
+        uint256 WeekNo,
+        uint256 Year
         );
 }
 
@@ -94,7 +87,9 @@ contract BundleBond is RBAC {
     mapping(address => Admin) AdminList;
     mapping(address => TeamOwner) TeamOwnerList;
     mapping(uint256 => Project) projectList;
+    mapping(uint256 => Offer) offerList;
     uint256 lastProjectId;
+    uint256 lastOfferId;
 
     struct Project {
         string name;
@@ -102,13 +97,22 @@ contract BundleBond is RBAC {
         address projectOwner;
     }
 
-    modifier onlyAdmin() {
-        hasRole(msg.sender, "Admin");
-        _;
+    struct Offer {
+        uint256 WeekId;
+        address fromAddress;
+        address ownerAddress;
+        bool accepted;
     }
-    modifier onlyTeamOwner() {
-        hasRole(msg.sender, "TEAM_OWNER");
-        _;
+
+
+
+    modifier  onlyAdmin() {
+       hasRole(msg.sender, "Admin");
+       _;
+    }
+    modifier  onlyTeamOwner() {
+       hasRole(msg.sender, "TEAM_OWNER");
+       _;
     }
 
     constructor(address _team, address _week) public {
@@ -117,7 +121,9 @@ contract BundleBond is RBAC {
         TeamContractAddress = _team;
         weekContractAddress = _team;
         lastProjectId = 0;
+        lastOfferId=0;
         addRole(msg.sender, "Admin");
+
     }
 
     struct Admin {
@@ -130,26 +136,36 @@ contract BundleBond is RBAC {
         bool exists;
     }
 
-    function isAdmin() public returns (bool) {
-        return hasRole(msg.sender, "Admin");
+    function  isAdmin() 
+    public
+    returns (bool)
+    {
+       return hasRole(msg.sender, "Admin");
     }
 
-    function addAdmin(string _adminName, address _adminAddress) {
+    function addAdmin(string _adminName, address _adminAddress) 
+    {
         Admin storage t = AdminList[_adminAddress];
         if (!t.exists) {
-            AdminList[_adminAddress] = Admin({exists: true, name: _adminName});
+            AdminList[_adminAddress] = Admin({
+                exists: true,
+                name: _adminName
+            });
             addRole(_adminAddress, "Admin");
         }
     }
 
-    function editAdminName(string _adminName, address _adminAddress) {
+    function editAdminName(string _adminName, address _adminAddress) 
+    {
         Admin storage t = AdminList[_adminAddress];
         if (t.exists) {
             AdminList[_adminAddress].name = _adminName;
         }
     }
 
-    function getAdminName(address _adminAddress) view returns (string) {
+    function getAdminName(address _adminAddress)  
+    view
+    returns (string){
         Admin storage admin = AdminList[_adminAddress];
         if (admin.exists) {
             return AdminList[_adminAddress].name;
@@ -157,11 +173,16 @@ contract BundleBond is RBAC {
         return "";
     }
 
-    function isTeamOwner() public returns (bool) {
-        return hasRole(msg.sender, "TEAM_OWNER");
+
+    function  isTeamOwner() 
+    public
+    returns(bool)
+    {
+       return hasRole(msg.sender, "TEAM_OWNER");
     }
 
-    function addTeamOwner(string _teamOwnerName, address _teamOwnerAddress) {
+    function addTeamOwner(string _teamOwnerName, address _teamOwnerAddress) 
+    {
         TeamOwner storage t = TeamOwnerList[_teamOwnerAddress];
         if (!t.exists) {
             TeamOwnerList[_teamOwnerAddress] = TeamOwner({
@@ -172,15 +193,16 @@ contract BundleBond is RBAC {
         }
     }
 
-    function editTeamOwnerName(string _teamOwnerName, address _teamOwnerAddress)
+    function editTeamOwnerName(string _teamOwnerName, address _teamOwnerAddress) 
     {
         TeamOwner storage t = TeamOwnerList[_teamOwnerAddress];
         if (t.exists) {
             TeamOwnerList[_teamOwnerAddress].name = _teamOwnerName;
         }
     }
-
-    function getTeamOwnerName(address _teamOwnerAddress) view returns (string) {
+    function getTeamOwnerName(address _teamOwnerAddress)  
+    view
+    returns (string){
         TeamOwner storage t = TeamOwnerList[_teamOwnerAddress];
         if (t.exists) {
             return TeamOwnerList[_teamOwnerAddress].name;
@@ -190,29 +212,25 @@ contract BundleBond is RBAC {
 
     function addTeam(
         address teamOwner,
-        string name,
-        string memory _temDes,
-        string memory _teamLoc,
-        string metadata
-    ) returns (uint256) {
-        return
-            TEAM.mintTeamToken(
-                teamOwner,
-                name,
-                _temDes,
-                _teamLoc,
-                metadata
-            );
+        string teamName,
+        string metadata)
+    returns(uint256)
+    {
+        return TEAM.mintTeamToken(teamOwner, teamName, metadata);
     }
-
-    function getTeamCount() public view returns (uint256 teamCount) {
+    
+    function getTeamCount() 
+    public 
+    view 
+    returns (uint256 teamCount)
+    {
         return TEAM.getTeamCount();
     }
 
-    function getTeamById(uint256 teamId)
-        public
-        view
-        returns (
+    function getTeamById(uint256 teamId) 
+    public 
+    view 
+    returns (
             uint256 id,
             address teamOwner,
             string name,
@@ -223,75 +241,114 @@ contract BundleBond is RBAC {
         return TEAM.getTeamById(teamId);
     }
 
-    function addWeek(
-        uint256 _teamTokenId,
-        address userAddress,
-        string metadata,
-        uint256 _WeekNo,
-        uint256 _Year
-    ) public {
-        WEEK.mint721(
-            TeamContractAddress,
-            _teamTokenId,
-            userAddress,
-            metadata,
-            _WeekNo,
-            _Year
-        );
+    function addWeek(uint256 _teamTokenId, address teamOwnerAddress) {
+        WEEK.mint721(TeamContractAddress, _teamTokenId, teamOwnerAddress);
     }
 
     function getWeekById(uint256 weekId)
         public
         view
         returns (
-            uint256 id,
-            uint256 teamId,
-            address weekOwner,
-            bool exists,
-            string WeekUrl,
-            uint256 price,
-            uint256 WeekNo,
-            uint256 Year
+        uint256 id,
+        uint256 teamId,
+        address weekOwner,
+        bool exists,
+        string WeekUrl,
+        uint256 price,
+        uint256 WeekNo,
+        uint256 Year
         )
     {
-        return WEEK.getWeekById(weekId);
+        return WEEK.getWeekById(weekId);    
     }
 
-    function purchaseWeekOfTeam(uint256 teamTokenId, uint256 weekTokenId)
-        public
-        payable
+
+
+
+    function purchaseWeekOfTeam(
+        uint256 teamTokenId,
+        uint256 weekTokenId)
+    public
+    payable
+    
     {
         //price to team
         //NFT update (set owner)
+        var ( teamId,teamOwner, name, TeamUrl, teamExists)=getTeamById(teamTokenId);
+        var ( weekId, weekTeamId, weekOwner, weekExists, WeekUrl, price,WeekNo,Year)=getWeekById(teamTokenId);
+        require(msg.value >= (price), "The value is " );
+
+        uint256 amountToRefund = msg.value - price; 
+        msg.sender.transfer(amountToRefund); 
+        weekOwner.transfer(price); 
+
+        sellWeekNFT(teamTokenId, weekTokenId,msg.sender );
     }
 
     function sellWeekNFT(
         uint256 teamTokenId,
         uint256 weekTokenId,
         address to
-    ) {
+    ) 
+    {
+
         TEAM.transferChild(teamTokenId, to, weekContractAddress, weekTokenId);
     }
 
-    function makeAnOffer(uint256 weekTokenId) public payable {
+
+    function makeAnOffer(
+        uint256 weekTokenId)
+    public
+    payable
+    {
         //price to contract
         //offer map update
-    }
+        var ( weekId, teamId, weekOwner, exists, WeekUrl, price,WeekNo,Year)=getWeekById(weekTokenId);
+        require(msg.value >= (price), "The value is " );
 
-    function acceptAnOffer(uint256 offerID) public payable {
+        uint256 amountToRefund = msg.value - price; 
+        msg.sender.transfer(amountToRefund); 
+
+        Offer storage o = offerList[lastOfferId];
+            offerList[lastOfferId] = Offer({
+                WeekId:weekTokenId,
+                fromAddress:msg.sender,
+                ownerAddress:weekOwner,
+                accepted:false
+        });
+        lastOfferId=lastOfferId+1;
+        
+    }
+    function acceptAnOffer(uint256 offerID)
+    public
+    payable
+    {
         //price to prev owner
-        //NFT update (owner update)
         //change offer status
         //call transferWeekNFTOnBid
+        var ( weekId, teamId, weekOwner, exists, WeekUrl, price,WeekNo,Year)=getWeekById(offerList[offerID].WeekId);
+        offerList[offerID].ownerAddress.transfer(price);
+        offerList[offerID].accepted = true;
+        transferWeekNFTOnBid( offerList[offerID].WeekId,offerList[offerID].fromAddress, offerList[offerID].ownerAddress );
     }
+
+    function rejectAnOffer(uint256 offerID)
+    public
+    payable
+    {
+        var ( weekId, teamId, weekOwner, exists, WeekUrl, price,WeekNo,Year)=getWeekById(offerList[offerID].WeekId);
+
+        offerList[offerID].fromTransfer.transfer(price);
+    }
+
 
     function transferWeekNFTOnBid(
         uint256 weekTokenId,
         address from,
         address to
     ) {
-        WEEK.transferWeekNFTOnBid(weekTokenId, from, to);
-    }
+        WEEK.transferWeekNFTOnBid(weekTokenId,from,to);
+    }    
 
     function placeBidOnWeekNFT(
         uint256 weekTokenId,
@@ -300,13 +357,14 @@ contract BundleBond is RBAC {
     ) public {
         WEEK.placeBid(weekTokenId, bidder, price);
     }
-
-    function AddProjectToResume(string _name, address _projectAddress) public {
-        projectList[lastProjectId] = Project({
-            name: _name,
-            projectAddress: _projectAddress,
-            projectOwner: msg.sender
-        });
-        lastProjectId = lastProjectId + 1;
+    function AddProjectToResume(string _name, address _projectAddress) 
+    public 
+    {
+            projectList[lastProjectId] = Project({
+                name: _name,
+                projectAddress:_projectAddress,
+                projectOwner:msg.sender
+            });
+            lastProjectId=lastProjectId+1;
     }
 }
